583f4859b20e05bdbed1fdc0bb4e4b97e9919d72
ve3 <kawewong@gmail.com>
2023-11-03 13:49:13 +0700

    v3.0.1

--- src/SimpleCache.php
+++ src/SimpleCache.php
@@ -11,13 +11,13 @@ namespace Rundiz\SimpleCache;
  * Rundiz SimpleCache
  * 
  * @since 3.0
- * @version 3.0
+ * @version 3.0.1
  */
 class SimpleCache
 {
 
 
-    const VERSION = '3.0';
+    const VERSION = '3.0.1';
 
 
 }
